a66b93ccc291a768ca91b0ca4de72ab8ae4f6216
XaquinAlves <xaquinalves@gmail.com>
2024-12-19 10:26:44 +0100

    First Commit

---
+++ 04-empregados.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF EXISTS empregados;
+
+CREATE DATABASE empregados CHARACTER SET=utf8mb4 COLLATE utf8mb4_unicode_520_ci;
+USE empregados;
+
+CREATE TABLE centro(
+    cenNumero INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    cenNome VARCHAR(100) NOT NULL,
+    cenEnderezo VARCHAR(255)
+);
+
+CREATE TABLE empregado(
+    empNumero INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    empDepartamento INT UNSIGNED NOT NULL,
+    empExtension INT UNSIGNED NOT NULL,
+    empDataNacemento DATE NOT NULL,
+    empDataIngreso DATE NOT NULL,
+    empSalario DECIMAL(10,2) UNSIGNED NOT NULL DEFAULT 0,
+    empComision DECIMAL(10,2) UNSIGNED NOT NULL DEFAULT 0,
+    empFillos SMALLINT UNSIGNED NOT NULL,
+    empNome VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE departamento(
+    depNumero INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    depNome VARCHAR(255) NOT NULL,
+    depDirector INT UNSIGNED NOT NULL,
+    depTipoDirector ENUM('P','F') NOT NULL,
+    depPresuposto DECIMAL(12,2) UNSIGNED NOT NULL CHECK(depPresuposto > 10000),
+    depDepende INT UNSIGNED,
+    depCentro INT UNSIGNED NOT NULL,
+    depEmpregados INT UNSIGNED NOT NULL DEFAULT 0
+);
+
+ALTER TABLE departamento
+    ADD CONSTRAINT departamento_FK_1 FOREIGN KEY(depCentro) REFERENCES centro(cenNumero) ON DELETE RESTRICT ON UPDATE CASCADE,
+    ADD CONSTRAINT departamento_FK_2 FOREIGN KEY(depDepende) REFERENCES departamento(depNumero) ON DELETE RESTRICT ON UPDATE CASCADE,
+    ADD CONSTRAINT departamento_FK FOREIGN KEY(depDirector) REFERENCES empregado(empNumero) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE empregado
+    ADD CONSTRAINT empregado_FK FOREIGN KEY(empDepartamento) REFERENCES departamento(depNumero) ON DELETE RESTRICT ON UPDATE CASCADE;
