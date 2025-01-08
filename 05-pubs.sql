f212498a2b1a0def9c8bc362759b014f931336d6
XaquinAlves <xaquinalves@gmail.com>
2025-01-08 09:18:25 +0100

    Correxido

--- 05-pubs.sql
+++ 05-pubs.sql
@@ -5,46 +5,46 @@ USE pubs;
 
 CREATE TABLE localidad(
     codLocalidad INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL
-);
+    nombre VARCHAR(255) NOT NULL
+)ENGINE=InnoDB;
 
 CREATE TABLE empleado(
     dniEmpleado CHAR(9) PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
     domicilio VARCHAR(255)
-);
+)ENGINE=InnoDB;
 
 CREATE TABLE pub(
-    codPub CHAR(20) PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    licenciaFiscal VARCHAR(255) NOT NULL,
+    codPub VARCHAR(30) PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    licenciaFiscal VARCHAR(30) NOT NULL,
     domicilio VARCHAR(255),
     fechaApertura DATE NOT NULL,
     horario ENUM('HOR1','HOR2','HOR3') NOT NULL,
     codLocalidad INT UNSIGNED NOT NULL
-);
+)ENGINE=InnoDB;
 
 CREATE TABLE pub_empleado(
-    codPub CHAR(20),
+    codPub VARCHAR(30),
     dniEmpleado CHAR(9),
     funcion ENUM('CAMARERO','SEGURIDAD','LIMPIEZA'),
     PRIMARY KEY(codPub,dniEmpleado,funcion)
-);
+)ENGINE=InnoDB;
 
 CREATE TABLE titular(
     dniTitular CHAR(9) PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     domicilio VARCHAR(255),
-    codPub CHAR(20) NOT NULL
-);
+    codPub VARCHAR(30) NOT NULL
+)ENGINE=InnoDB;
 
 CREATE TABLE existencias(
     codArticulo CHAR(10) PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     cantidad INT UNSIGNED NOT NULL,
     precio DECIMAL(7,2) UNSIGNED NOT NULL CHECK(precio > 0),
-    codPub CHAR(20) NOT NULL
-);
+    codPub VARCHAR(30) NOT NULL
+)ENGINE=InnoDB;
 
 ALTER TABLE pub
     ADD FOREIGN KEY(codLocalidad) REFERENCES localidad(codLocalidad) ON DELETE RESTRICT ON UPDATE CASCADE;
